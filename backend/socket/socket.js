de58023d29d96968c01fcf40d2078daf1c265cda
ashique6465 <mdashiqueali71@gmail.com>
2024-09-12 17:50:27 +0530

    Deploy

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin:["http://localhost:3000"],
+        origin:["https://one-chat-lime.vercel.app/"],
         methods:["GET","POST"]
     }
 })
